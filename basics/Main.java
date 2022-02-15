638ed7e3e79b46c298714e62643c1f4b90bcdf3d
Ben Mills <fuzzyllama@gmail.com>
2022-02-14 16:02:47 -0800

    Adds flipping coins feature

--- basics/Main.java
+++ basics/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
@@ -11,6 +11,9 @@ public class Main {
 		int turtleCount = 0;
 		System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
 		System.out.print("\n\n");
+
+		System.out.println("~FLIPPING COINS~");
+		flipNHeads(3);
 	}
 
 	public static String pluralize(String str, int num) {
@@ -19,6 +22,19 @@ public class Main {
 	}
 
 	public static void flipNHeads(int n) {
-
+		Random rand = new Random();
+		int headCount = 0;
+		int flips = 0;
+		while(headCount < n) {
+			flips++;
+			if (rand.nextFloat() >= 0.5) { // nextFloat() gives us a float beteen 0.0 and 1.0;
+				headCount++;
+				System.out.println("heads");
+			} else {
+				headCount = 0;
+				System.out.println("tails");
+			}
+		}
+		System.out.println("It took " + flips + " flips to flip " + n + " heads in a row.");
 	}
 }
