a4eca7223c5dc86b5b7cb6472b1c5c195303abf9
Ben Mills <fuzzyllama@gmail.com>
2022-02-20 11:29:17 -0800

    Adds initial code for lower level approach

---
+++ bitmapTransformer/app/src/main/java/bitmapTransformer/bitmap/Bitmap.java
@@ -0,0 +1,14 @@
+package bitmapTransformer.bitmap;
+
+import java.io.File;
+
+public class Bitmap {
+    int width;
+    int height;
+    byte[] header = new byte[54];
+    byte[][] pixelData;
+
+    Bitmap(File imageFile) {
+       // TODO
+    }
+}
