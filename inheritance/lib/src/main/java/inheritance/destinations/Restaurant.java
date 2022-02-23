a51d27fbba450ce2b0efa223684b374b3950277e
Ben Mills <fuzzyllama@gmail.com>
2022-02-22 19:59:46 -0800

    Adds Destination class, refactors to make restaurants subclass. Also adds shop class with unit test

--- inheritance/lib/src/main/java/inheritance/destinations/Restaurant.java
+++ inheritance/lib/src/main/java/inheritance/destinations/Restaurant.java
@@ -1,76 +1,13 @@
 package inheritance.destinations;
 
-import inheritance.User;
-import inheritance.review.Review;
-import inheritance.review.Reviewable;
-
 import java.util.ArrayList;
 
-public class Restaurant implements Reviewable {
-    private final String name;
-    private int priceScore;
-    private int reviewCount = 0;
-    private final ArrayList<Review> reviews;
+public class Restaurant extends Destination {
 
     public Restaurant(String name, int priceScore) {
+        super(name, priceScore);
         this.name = name;
-        setPriceScore(priceScore);
         reviews = new ArrayList<>();
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getStarScore() {
-        if(reviews.size() > 0)
-            return getAverageReviewStarScore(reviews);
-        else
-            return 0;
-    }
-
-    public int getPriceScore() {
-        return priceScore;
-    }
-
-    public void setPriceScore(int priceScore) {
-        if(priceScore < 0 || priceScore > 3)
-            throw new IllegalArgumentException("Star score " + getStarScore() + " out of range.");
-        this.priceScore = priceScore;
-    }
-
-    public void addReview(Review review) {
-        if(!reviews.contains(review)) {
-            reviews.add(review);
-            reviewCount++;
-        }
-    }
-
-    public void updateReviewStars(User author, int newStars) {
-        for(Review element : reviews) {
-            if(element.getAuthor().equals(author))
-                element.setStarScore(newStars);
-        }
-    }
-
-    private int getAverageReviewStarScore(ArrayList<Review> list) {
-        int sum = 0;
-       for(Review element : list) {
-          sum += element.getStarScore();
-       }
-       return sum / list.size();
-    }
-
-    public int getReviewCount() {
-        return reviewCount;
-    }
-
-    @Override
-    public String toString() {
-        return "{ name: " + name + ", stars: " + getStarScore() + ", price: " + priceScore + " }";
-    }
-
-
-
 
 }
--- inheritance/lib/src/main/java/inheritance/destinations/Shop.java
+++ inheritance/lib/src/main/java/inheritance/destinations/Shop.java
@@ -1,18 +1,41 @@
 package inheritance.destinations;
 
-public class Shop {
-    String name;
-    String description;
-    int priceScore;
+import inheritance.User;
+import inheritance.review.Review;
+import inheritance.review.Reviewable;
+
+import java.util.ArrayList;
+
+public class Shop extends Destination {
+    private final String description;
 
     public Shop(String name, String description, int priceScore) {
-        this.name = name;
+        super(name, priceScore);
         this.description = description;
-        this.priceScore = priceScore;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     @Override
     public String toString() {
         return "{ name: " + name + ", description: " + description + ", price: " + priceScore + " }";
+    }
+
+    @Override
+    public void addReview(Review review) {
+        if(!reviews.contains(review)) {
+            reviews.add(review);
+            reviewCount++;
+        }
+    }
+
+    @Override
+    public void updateReviewStars(User author, int newStars) {
+        for(Review element : reviews) {
+            if(element.getAuthor().equals(author))
+                element.setStarScore(newStars);
+        }
     }
 }
